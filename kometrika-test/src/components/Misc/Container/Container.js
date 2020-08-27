430dc4a29ac70f4524e011c60beff2b150dcff9d
nat-davydova <natti.davydova@gmail.com>
2020-08-27 13:25:57 +0300

    Footer: container prop types

--- kometrika-test/src/components/Misc/Container/Container.js
+++ kometrika-test/src/components/Misc/Container/Container.js
@@ -1,9 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./Container.module.scss";
 
 const container = ({ children }) => (
   <div className={styles.container}>{children}</div>
 );
+
+container.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node.isRequired,
+    PropTypes.element.isRequired
+  ])
+};
 
 export default container;
