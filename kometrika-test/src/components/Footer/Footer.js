2971146063de26e6b2161ad3e2f95cd48e969616
nat-davydova <natti.davydova@gmail.com>
2020-08-27 13:15:44 +0300

    Footer: container component, structure

--- kometrika-test/src/components/Footer/Footer.js
+++ kometrika-test/src/components/Footer/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const footer = props => <footer>FOOTER</footer>;
+import Container from "components/Misc/Container/Container";
+
+const footer = props => (
+  <footer>
+    <Container>
+      <p>Natalia Davydova, 2020</p>
+    </Container>
+  </footer>
+);
 
 export default footer;
---
+++ kometrika-test/src/components/Misc/Container/Container.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const container = ({ children }) => <div>{children}</div>;
+
+export default container;
