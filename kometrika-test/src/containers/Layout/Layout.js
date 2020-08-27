4742a878913884e6cb635970871ecd1d0c5e68b2
nat-davydova <natti.davydova@gmail.com>
2020-08-27 14:02:52 +0300

    Nav: basic component

--- kometrika-test/src/containers/Layout/Layout.js
+++ kometrika-test/src/containers/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 
+import Nav from "components/Nav/Nav";
 import Footer from "components/Footer/Footer";
 
 import styles from "./Layout.module.scss";
@@ -8,7 +9,7 @@ class Layout extends Component {
   render() {
     return (
       <Fragment>
-        <div>NAV</div>
+        <Nav />
         <div>MAIN</div>
         <Footer
           classes={{
