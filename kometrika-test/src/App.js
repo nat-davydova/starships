6fc71dac89567c0b1a841575e4a2f6d6c9f6850a
nat-davydova <natti.davydova@gmail.com>
2020-08-27 12:55:14 +0300

    Layout: basic component

--- kometrika-test/src/App.js
+++ kometrika-test/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+
+import Layout from "./containers/Layout/Layout";
+
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
 }
 
 export default App;
