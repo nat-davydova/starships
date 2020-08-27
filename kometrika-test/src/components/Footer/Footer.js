27be4a536eb338621b34d2233c6b8d948c30ecb3
nat-davydova <natti.davydova@gmail.com>
2020-08-27 13:41:40 +0300

    Footer: sticky footer styles

--- kometrika-test/src/components/Footer/Footer.js
+++ kometrika-test/src/components/Footer/Footer.js
@@ -1,15 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
 import Container from "components/Misc/Container/Container";
 
 import styles from "./Footer.module.scss";
 
-const footer = props => (
-  <footer className={styles.footer}>
-    <Container>
-      <p>Natalia Davydova, 2020</p>
-    </Container>
-  </footer>
-);
+const footer = ({ classes = {} }) => {
+  const { root = "" } = classes;
+
+  const footerClassnames = classnames(styles.footer, root);
+
+  return (
+    <footer className={footerClassnames}>
+      <Container>
+        <p>Natalia Davydova, 2020</p>
+      </Container>
+    </footer>
+  );
+};
+
+footer.propTypes = {
+  classes: PropTypes.object
+};
 
 export default footer;
--- kometrika-test/src/containers/Layout/Layout.js
+++ kometrika-test/src/containers/Layout/Layout.js
@@ -2,13 +2,19 @@ import React, { Component, Fragment } from "react";
 
 import Footer from "components/Footer/Footer";
 
+import styles from "./Layout.module.scss";
+
 class Layout extends Component {
   render() {
     return (
       <Fragment>
         <div>NAV</div>
         <div>MAIN</div>
-        <Footer />
+        <Footer
+          classes={{
+            root: styles.footer
+          }}
+        />
       </Fragment>
     );
   }
