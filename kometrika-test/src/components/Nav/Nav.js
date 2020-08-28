8c897796b992159e4985fb7ddd3e6fd143f79c2e
nat-davydova <natti.davydova@gmail.com>
2020-08-28 11:39:06 +0300

    Nav: basic styles, inner styles

--- kometrika-test/src/components/Nav/Nav.js
+++ kometrika-test/src/components/Nav/Nav.js
@@ -2,6 +2,14 @@ import React from "react";
 
 import Container from "components/Misc/Container/Container";
 
-const nav = props => <Container>Nav</Container>;
+import styles from "./Nav.module.scss";
+
+const nav = props => (
+  <nav className={styles.nav}>
+    <Container>
+      <div className={styles.nav_inner}>Nav</div>
+    </Container>
+  </nav>
+);
 
 export default nav;
