1906cd1807214c5b445809a0606c40728ad07dd0
nat-davydova <natti.davydova@gmail.com>
2020-08-28 15:39:41 +0300

    Comparison Table: section - subtitle prop types

--- kometrika-test/src/components/Section/SectionSubtitle/SectionSubtitle.js
+++ kometrika-test/src/components/Section/SectionSubtitle/SectionSubtitle.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./SectionSubtitle.module.scss";
 
 const sectionSubtitle = ({ children }) => (
   <h2 className={styles.subtitle}>{children}</h2>
 );
+
+sectionSubtitle.propTypes = {
+  children: PropTypes.string.isRequired
+};
 
 export default sectionSubtitle;
