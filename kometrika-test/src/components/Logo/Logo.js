7c07cf8509295b9e770789e28973c4c19af359a4
nat-davydova <natti.davydova@gmail.com>
2020-08-28 12:02:39 +0300

    Nav: logo - styles

--- kometrika-test/src/components/Logo/Logo.js
+++ kometrika-test/src/components/Logo/Logo.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import { ReactComponent as LogoImg } from "assets/img/logo.svg";
 
+import styles from "./Logo.module.scss";
+
 const logo = props => (
-  <div>
-    <LogoImg />
-    <span>Star Wars Spaceships Comparison</span>
+  <div className={styles.logo}>
+    <LogoImg className={styles.logo_img} />
+    <span className={styles.logo_txt}>Star Wars Spaceships Comparison</span>
   </div>
 );
 
