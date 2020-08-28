9365f09c418d7082d805a330cc07fa16442f58a2
nat-davydova <natti.davydova@gmail.com>
2020-08-28 12:28:12 +0300

    Main: componebt structure

--- kometrika-test/src/components/Main/Main.js
+++ kometrika-test/src/components/Main/Main.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const main = props => <main>Main</main>;
+import Container from "components/Misc/Container/Container";
+
+const main = ({ children }) => (
+  <main>
+    <Container>
+      <div className="">Main 2{children}</div>
+    </Container>
+  </main>
+);
 
 export default main;
