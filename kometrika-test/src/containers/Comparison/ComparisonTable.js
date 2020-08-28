61a1f2861f773323130926814a62f2e4055d099e
nat-davydova <natti.davydova@gmail.com>
2020-08-28 14:10:28 +0300

    Comparison Table: adding section configs

--- kometrika-test/src/containers/Comparison/ComparisonTable.js
+++ kometrika-test/src/containers/Comparison/ComparisonTable.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { sectionConfig } from "./config";
+
 class ComparisonTable extends Component {
   render() {
     return <div></div>;
---
+++ kometrika-test/src/containers/Comparison/config.js
@@ -0,0 +1,4 @@
+export const sectionConfig = {
+  title: "Spaceships Comparison Table",
+  subtitle: "Check which one is better!"
+};
