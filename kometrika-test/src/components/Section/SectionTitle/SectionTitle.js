4dbbd8dcc45f4b629f2d36310b4d050ed6595075
nat-davydova <natti.davydova@gmail.com>
2020-08-28 14:56:27 +0300

    Comparison Table: section - prop types

--- kometrika-test/src/components/Section/SectionTitle/SectionTitle.js
+++ kometrika-test/src/components/Section/SectionTitle/SectionTitle.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./SectionTitle.module.scss";
 
 const sectionTitle = ({ children }) => (
   <h1 className={styles.title}>{children}</h1>
 );
+
+sectionTitle.propTypes = {
+  children: PropTypes.string.isRequired
+};
 
 export default sectionTitle;
