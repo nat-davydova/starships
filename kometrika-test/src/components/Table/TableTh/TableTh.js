22073894397ce55f24ab9045ed4c84ce54deb66a
nat-davydova <natti.davydova@gmail.com>
2020-08-28 18:08:16 +0300

    Comparison Table: table - outsourcing th to a separate component

---
+++ kometrika-test/src/components/Table/TableTh/TableTh.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const tableTh = ({ children }) => <th>{children}</th>;
+
+export default tableTh;
--- kometrika-test/src/components/Table/TableThead/TableThead.js
+++ kometrika-test/src/components/Table/TableThead/TableThead.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TableTr from "components/Table/TableTr/TableTr";
+import TableTh from "components/Table/TableTh/TableTh";
 
 import StarDestroyerImg from "assets/img/starships/star-destroyer.png";
 import FalconImg from "assets/img/starships/falcon.png";
@@ -8,21 +9,21 @@ import FalconImg from "assets/img/starships/falcon.png";
 const tableThead = props => (
   <thead>
     <TableTr>
-      <th></th>
-      <th>
+      <TableTh></TableTh>
+      <TableTh>
         <img src={StarDestroyerImg} />
         <p>Star Destroyer</p>
         <p>Imperial I-class Star Destroyer</p>
         <p>(Star Destroyer)</p>
         <p>Kuat Drive Yards</p>
-      </th>
-      <th>
+      </TableTh>
+      <TableTh>
         <img src={FalconImg} />
         <p>Millennium Falcon</p>
         <p>YT-1300 light freighter</p>
         <p>(Light freighter)</p>
         <p>Corellian Engineering Corporation</p>
-      </th>
+      </TableTh>
     </TableTr>
   </thead>
 );
