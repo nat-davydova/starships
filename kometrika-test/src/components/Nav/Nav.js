509299da30dacc3cbabdd46ddbe40da5f3270e21
nat-davydova <natti.davydova@gmail.com>
2020-08-28 11:43:46 +0300

    Nav: logo - basic component

--- kometrika-test/src/components/Nav/Nav.js
+++ kometrika-test/src/components/Nav/Nav.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Container from "components/Misc/Container/Container";
+import Logo from "components/Logo/Logo";
 
 import styles from "./Nav.module.scss";
 
 const nav = props => (
   <nav className={styles.nav}>
     <Container>
-      <div className={styles.nav_inner}>Nav</div>
+      <div className={styles.nav_inner}>
+        <Logo />
+      </div>
     </Container>
   </nav>
 );
