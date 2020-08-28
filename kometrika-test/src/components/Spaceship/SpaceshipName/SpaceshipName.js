f26e5e2bbfcbb33c8dcefab0f13ecf53941ca68f
nat-davydova <natti.davydova@gmail.com>
2020-08-28 18:30:49 +0300

    Comparison Table: table - spaceship name prop types

--- kometrika-test/src/components/Spaceship/SpaceshipName/SpaceshipName.js
+++ kometrika-test/src/components/Spaceship/SpaceshipName/SpaceshipName.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./SpaceshipName.module.scss";
 
 const spaceshipName = ({ name }) => <p className={styles.name}>{name}</p>;
+
+spaceshipName.propTypes = {
+  name: PropTypes.string.isRequired
+};
 
 export default spaceshipName;
