76cfb8b50077752825993921d2e94d93cecda60c
nat-davydova <natti.davydova@gmail.com>
2020-08-28 17:17:47 +0300

    Comparison Table: table - adding images to thead

--- kometrika-test/src/components/Table/Table.js
+++ kometrika-test/src/components/Table/Table.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import styles from "./Table.module.scss";
 
+import StarDestroyerImg from "assets/img/starships/star-destroyer.png";
+import FalconImg from "assets/img/starships/falcon.png";
+
 const table = props => (
   <div className={styles.tableContainer}>
     <table className={styles.table}>
@@ -9,12 +12,14 @@ const table = props => (
         <tr>
           <th></th>
           <th>
+            <img src={StarDestroyerImg} />
             <p>Star Destroyer</p>
             <p>Imperial I-class Star Destroyer</p>
             <p>(Star Destroyer)</p>
             <p>Kuat Drive Yards</p>
           </th>
           <th>
+            <img src={FalconImg} />
             <p>Millennium Falcon</p>
             <p>YT-1300 light freighter</p>
             <p>(Light freighter)</p>
