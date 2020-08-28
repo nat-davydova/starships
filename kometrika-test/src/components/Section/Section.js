7d12a255ee2c9657342880c41479f7514bf2992e
nat-davydova <natti.davydova@gmail.com>
2020-08-28 14:30:24 +0300

    Comparison Table: section - basic structure

--- kometrika-test/src/components/Section/Section.js
+++ kometrika-test/src/components/Section/Section.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-const section = ({ sectionConfig }) => <section></section>;
+const section = ({ sectionConfig }) => {
+  const { title, subtitle } = sectionConfig;
+
+  return (
+    <section>
+      <header>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+      </header>
+      <div>Section Content Is Here</div>
+    </section>
+  );
+};
 
 export default section;
