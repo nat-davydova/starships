7321ad064e45ecd1c27046b3dadb70ec3c72dff2
nat-davydova <natti.davydova@gmail.com>
2020-08-28 14:06:21 +0300

    Comparison Table: creating component

--- kometrika-test/src/App.js
+++ kometrika-test/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Layout from "./containers/Layout/Layout";
+import ComparisonTable from "./containers/Comparison/ComparisonTable";
 
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Layout>
+        <ComparisonTable />
+      </Layout>
     </div>
   );
 }
