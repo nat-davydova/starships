5589f882d1ea8f2b0c04b2d6bfc7a66d45c30c69
nat-davydova <natti.davydova@gmail.com>
2020-08-28 11:21:48 +0300

    Nav: adding container, small container styles fix

--- kometrika-test/src/components/Nav/Nav.js
+++ kometrika-test/src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const nav = (props) => (
+import Container from "components/Misc/Container/Container";
 
-);
+const nav = props => <Container>Nav</Container>;
 
 export default nav;
