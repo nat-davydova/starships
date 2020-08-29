72924c7dfa6f53b2375a4324d107110c445c472f
nat-davydova <natti.davydova@gmail.com>
2020-08-29 15:26:40 +0300

    Comparison table: rendering table - removing tbody test data

--- kometrika-test/src/components/Table/TableTbody/TableTbody.js
+++ kometrika-test/src/components/Table/TableTbody/TableTbody.js
@@ -4,20 +4,9 @@ import PropTypes from "prop-types";
 import TableTr from "components/Table/TableTr/TableTr";
 import TableTd from "components/Table/TableTd/TableTd";
 
-const tableTbody = ({ config }) => (
-  <tbody>
-    <TableTr>
-      <TableTd isCriteria>Cost (in credits)</TableTd>
-      <TableTd>150000000</TableTd>
-      <TableTd>100000</TableTd>
-    </TableTr>
-    <TableTr>
-      <TableTd isCriteria>Length</TableTd>
-      <TableTd>1,600</TableTd>
-      <TableTd>34.37</TableTd>
-    </TableTr>
-  </tbody>
-);
+const tableTbody = ({ config }) => {
+  return <tbody></tbody>;
+};
 
 tableTbody.propTypes = {
   config: PropTypes.array.isRequired
