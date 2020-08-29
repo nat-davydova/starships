8f555420bb1ce3de1851ef219f8977daaa8e23ae
nat-davydova <natti.davydova@gmail.com>
2020-08-29 14:38:39 +0300

    Comparison table: table - starship img component styles

--- kometrika-test/src/components/Misc/Img/Img.js
+++ kometrika-test/src/components/Misc/Img/Img.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const img = ({ src, alt = "" }) => <img src={src} alt={alt} />;
+const img = ({ src, alt = "", classes = {} }) => {
+  const { root = "" } = classes;
+
+  return <img className={root} src={src} alt={alt} />;
+};
 
 img.propTypes = {
   src: PropTypes.string.isRequired,
-  alt: PropTypes.string
+  alt: PropTypes.string,
+  classes: PropTypes.object
 };
 
 export default img;
--- kometrika-test/src/components/Starship/StarshipImg/StarshipImg.js
+++ kometrika-test/src/components/Starship/StarshipImg/StarshipImg.js
@@ -3,8 +3,16 @@ import PropTypes from "prop-types";
 
 import Img from "components/Misc/Img/Img";
 
+import styles from "./StarshipImg.module.scss";
+
 const starshipImg = ({ imgSrc, imgAlt = "" }) => (
-  <Img src={imgSrc} alt={imgAlt} />
+  <Img
+    src={imgSrc}
+    alt={imgAlt}
+    classes={{
+      root: styles.img
+    }}
+  />
 );
 
 starshipImg.propTypes = {
