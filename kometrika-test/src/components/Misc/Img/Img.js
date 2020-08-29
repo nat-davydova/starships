328fb0da6565135e1e231a9c067eda6cf6e63898
nat-davydova <natti.davydova@gmail.com>
2020-08-29 14:16:47 +0300

    Comparison table: table - img component prop types

--- kometrika-test/src/components/Misc/Img/Img.js
+++ kometrika-test/src/components/Misc/Img/Img.js
@@ -1,5 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const img = ({ src, alt }) => <img src={src} alt={alt} />;
+const img = ({ src, alt = "" }) => <img src={src} alt={alt} />;
+
+img.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string
+};
 
 export default img;
