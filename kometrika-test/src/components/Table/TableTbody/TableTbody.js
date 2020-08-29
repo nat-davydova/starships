5a3c4c5a0eebbf2c71135ba472adead6eb0e1b95
nat-davydova <natti.davydova@gmail.com>
2020-08-29 15:00:23 +0300

    Comparison table: table - tbody rendering with couple of test tr components

--- kometrika-test/src/components/Table/TableTbody/TableTbody.js
+++ kometrika-test/src/components/Table/TableTbody/TableTbody.js
@@ -1,7 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const tableTbody = ({ config }) => <tbody></tbody>;
+import TableTr from "components/Table/TableTr/TableTr";
+
+const tableTbody = ({ config }) => (
+  <tbody>
+    <TableTr>
+      <td>
+        <strong>Cost (in credits)</strong>
+      </td>
+      <td>150000000</td>
+      <td>100000</td>
+    </TableTr>
+    <TableTr>
+      <td>
+        <strong>Length</strong>
+      </td>
+      <td>1,600</td>
+      <td>34.37</td>
+    </TableTr>
+  </tbody>
+);
 
 tableTbody.propTypes = {
   config: PropTypes.array.isRequired
