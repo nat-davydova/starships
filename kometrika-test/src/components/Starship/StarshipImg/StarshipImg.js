f27a52fe13924e516f9e083a19a35c196bbaf3c3
nat-davydova <natti.davydova@gmail.com>
2020-08-29 14:35:29 +0300

    Comparison table: table - starship img component props

--- kometrika-test/src/components/Starship/StarshipImg/StarshipImg.js
+++ kometrika-test/src/components/Starship/StarshipImg/StarshipImg.js
@@ -1,7 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Img from "components/Misc/Img/Img";
 
-const starshipImg = ({ imgSrc, imgAlt }) => <Img src={imgSrc} alt={imgAlt} />;
+const starshipImg = ({ imgSrc, imgAlt = "" }) => (
+  <Img src={imgSrc} alt={imgAlt} />
+);
+
+starshipImg.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string
+};
 
 export default starshipImg;
