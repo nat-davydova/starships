3abae8704c26c9a987ca49737dcbea6a2d7fb87c
nat-davydova <natti.davydova@gmail.com>
2020-08-29 16:27:03 +0300

    Comparison table: rendering table - basic rendering from temp config

--- kometrika-test/src/components/Table/TableTbody/TableTbody.js
+++ kometrika-test/src/components/Table/TableTbody/TableTbody.js
@@ -5,7 +5,21 @@ import TableTr from "components/Table/TableTr/TableTr";
 import TableTd from "components/Table/TableTd/TableTd";
 
 const tableTbody = ({ config }) => {
-  return <tbody></tbody>;
+  const tableData = [];
+  const defaultConfigElem = config[0];
+
+  for (let configKey in defaultConfigElem) {
+    const trData = [];
+    trData.push(<TableTd isCriteria>{configKey}</TableTd>);
+
+    config.forEach(configElem => {
+      trData.push(<TableTd>{configElem[configKey]}</TableTd>);
+    });
+
+    tableData.push(<TableTr>{trData}</TableTr>);
+  }
+
+  return <tbody>{tableData}</tbody>;
 };
 
 tableTbody.propTypes = {
