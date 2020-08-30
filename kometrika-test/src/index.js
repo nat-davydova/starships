6ce6141655d9268fc51f77e5c25e65c80c48ac46
nat-davydova <natti.davydova@gmail.com>
2020-08-30 13:09:40 +0300

    Redux preps: customizing redux, applying middleware

--- kometrika-test/src/index.js
+++ kometrika-test/src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+
 import App from "./App";
+
+import reducer from "./store/reducer";
+
+import "./index.scss";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
