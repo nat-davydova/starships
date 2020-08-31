abc250d5530b62e2dfdf23020412bf8e74286eb1
nat-davydova <natti.davydova@gmail.com>
2020-08-31 12:54:06 +0300

    Comparison table: minmax table values - outsourcing transforming data into separate functions

--- kometrika-test/src/store/reducer/utils.js
+++ kometrika-test/src/store/reducer/utils.js
@@ -29,10 +29,56 @@ const _starshipsFullData = (criteriaArr, shipsArr) => {
   return fullData;
 };
 
+// transforming plain numerical data
+// (removing ',' symbols, ignoring NaN data)
+// additional separator can be any units, like 'km' or 'kg' etc
+const _transformNumericalData = (dataArr, additionalSeparator) => {
+  const newDataArr = [];
+
+  dataArr.forEach((elem, index) => {
+    if (additionalSeparator) {
+      elem = elem.split(additionalSeparator)[0];
+    }
+
+    elem = elem
+      .toString()
+      .split(",")
+      .join("");
+    if (!isNaN(parseFloat(elem))) {
+      newDataArr.push({ [`${index}`]: parseFloat(elem) });
+    }
+  });
+
+  return newDataArr;
+};
+
+// consumables data transforming
+// weeks, months, years -> to days for simpler comparison
+// week -> 7 days, month -> 30 days, year -> 365 days
+export const _transformConsumablesData = dataArr => {
+  const newDataArr = dataArr.map((elem, index) => {
+    const [numVal, units] = elem.split(" ");
+    let newNumVal = parseInt(numVal);
+
+    if (units === "week" || units === "weeks") {
+      newNumVal *= 7;
+    } else if (units === "month" || units === "months") {
+      newNumVal *= 30;
+    } else if (units === "year" || units === "years") {
+      newNumVal *= 365;
+    }
+
+    return { [`${index}`]: newNumVal };
+  });
+
+  return newDataArr;
+};
+
 export const starshipsCriteriaMinMax = (criteriaArr, shipsArr) => {
   const fullData = _starshipsFullData(criteriaArr, shipsArr);
 
   const minMaxVals = {};
+  let dataArr = [];
 
   for (const criteria in fullData) {
     if (
@@ -44,56 +90,27 @@ export const starshipsCriteriaMinMax = (criteriaArr, shipsArr) => {
       criteria === "hyperdrive_rating" ||
       criteria === "MGLT"
     ) {
-      const dataArr = [];
-      fullData[criteria].forEach(elem => {
-        elem = elem
-          .toString()
-          .split(",")
-          .join("");
-        if (!isNaN(parseFloat(elem))) {
-          dataArr.push(elem);
-        }
-      });
+      dataArr = _transformNumericalData(fullData[criteria]);
 
-      const min = Math.min(...dataArr);
-      const max = Math.max(...dataArr);
-
-      minMaxVals[criteria] = [min, max];
+      // const min = Math.min(...dataArr);
+      // const max = Math.max(...dataArr);
+      //
+      // minMaxVals[criteria] = [min, max];
     } else if (criteria === "max_atmosphering_speed") {
-      const dataArr = [];
-      fullData[criteria].forEach(elem => {
-        elem = elem.toString().split("km");
-        if (!isNaN(parseFloat(elem[0]))) {
-          dataArr.push(parseFloat(elem[0]));
-        }
-      });
+      dataArr = _transformNumericalData(fullData[criteria], "km");
 
-      const min = Math.min(...dataArr);
-      const max = Math.max(...dataArr);
-
-      minMaxVals[criteria] = [min, max];
+      // const min = Math.min(...dataArr);
+      // const max = Math.max(...dataArr);
+      //
+      // minMaxVals[criteria] = [min, max];
     } else if (criteria === "consumables") {
-      const dataArr = fullData[criteria].map(elem => {
-        const [numVal, units] = elem.split(" ");
-        let newNumVal = parseInt(numVal);
-
-        if (units === "week" || units === "weeks") {
-          newNumVal *= 7;
-        } else if (units === "month" || units === "months") {
-          newNumVal *= 30;
-        } else if (units === "year" || units === "years") {
-          newNumVal *= 365;
-        }
-
-        return newNumVal;
-      });
-
-      const min = Math.min(...dataArr);
-      const max = Math.max(...dataArr);
-
-      minMaxVals[criteria] = [min, max];
+      dataArr = _transformConsumablesData(fullData["consumables"]);
+      // const min = Math.min(...dataArr);
+      // const max = Math.max(...dataArr);
+      //
+      // minMaxVals[criteria] = [min, max];
     }
+    console.log(dataArr);
   }
-
-  console.log(minMaxVals);
+  //console.log(minMaxVals);
 };
