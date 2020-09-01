b9c867dca0e1a0b18ea61e21ce2cbe7c006823df
nat-davydova <natti.davydova@gmail.com>
2020-09-01 17:13:49 +0300

    Starships: redux settings - combining 2 reducers

--- kometrika-test/src/index.js
+++ kometrika-test/src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 import reducer from "./store/reducer/reducer";
+import starshipsReducer from "./store/reducer/starships/starships";
 
 import "./index.scss";
 
+const rootReducer = combineReducers({
+  //here we define different areas of the app and apply reducers to them
+  starships: starshipsReducer,
+  comparison: reducer
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducer,
+  rootReducer,
   /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
 );
 
