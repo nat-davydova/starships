f0849aeaf4eaa2e2e899b502ef9f4af0ce3a3217
nat-davydova <natti.davydova@gmail.com>
2020-09-01 17:36:39 +0300

    Starships: rendering ships - mapping component to the store, grabbing data from server

--- kometrika-test/src/containers/Starships/Starships.js
+++ kometrika-test/src/containers/Starships/Starships.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Section from "components/Section/Section";
 import RoutingBtn from "components/UI/RoutingBtn/RoutingBtn";
 import StarshipsGrid from "components/StarshipsGrid/StarshipsGrid";
 
 import { sectionConfig } from "./config";
+import { grabbingStarships } from "store/actions/actions";
 
 class Starships extends Component {
+  async componentDidMount() {
+    // pass film number here
+    // Star Wars V - is equal to '2' num in the SWAPI API
+    await this.props.onGrabbingStarships(2);
+  }
+
   render() {
     return (
       <Section sectionConfig={sectionConfig}>
@@ -17,4 +25,18 @@ class Starships extends Component {
   }
 }
 
-export default Starships;
+const mapStateToProps = state => {
+  return {
+    starships: state.starships.starships,
+    isError: state.starships.isError,
+    errorTxt: state.starships.errorTxt
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onGrabbingStarships: elem => dispatch(grabbingStarships(elem))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Starships);
