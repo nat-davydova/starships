1b67dc7bc8694997e744e204158eaf5b5e0008f0
nat-davydova <natti.davydova@gmail.com>
2020-09-01 16:05:55 +0300

    Starships: starship card component and single card component

---
+++ kometrika-test/src/components/Card/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const card = props => {
+  return <div></div>;
+};
+
+card.propTypes = {};
+
+export default card;
---
+++ kometrika-test/src/components/StarshipsGrid/StarshipCard/StarshipCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const starshipCard = props => {
+  return <div></div>;
+};
+
+starshipCard.propTypes = {};
+
+export default starshipCard;
