21a288738d53dae7f9df8b83ebf87d31945e464f
nat-davydova <natti.davydova@gmail.com>
2020-09-01 17:25:46 +0300

    Starships: redux settings - adding starships reducer and actions files

--- kometrika-test/src/store/reducer/starships/starships.js
+++ kometrika-test/src/store/reducer/starships/starships.js
@@ -1,7 +1,40 @@
-const initState = {};
+import * as actionTypes from "store/actions/actionTypes";
+import { starshipConfigAddImgs } from "../utils";
+
+const initState = {
+  starships: [],
+  isError: false,
+  errorTxt: ""
+};
 
 const starshipsReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.STARSHIPS_SUCCESS:
+      const starships = state.starships.concat(action.payload.config);
+
+      const starshipsWithImgs = starships.map(elem =>
+        starshipConfigAddImgs(elem)
+      );
+
+      return {
+        ...state,
+        isError: false,
+        errorTxt: "",
+        starships: starshipsWithImgs
+      };
+
+    case actionTypes.STARSHIPS_ERROR:
+      const errorTxt = action.payload.errorTxt;
+
+      return {
+        ...state,
+        isError: true,
+        errorTxt: errorTxt
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default starshipsReducer;
